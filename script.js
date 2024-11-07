8857177d25d12d68760d2ed49be1bd865d5cd0cb
wamellx <wamellx@gmail.com>
2024-11-07 17:42:42 -0500

    Sets up base variables and playRound function

---
+++ script.js
@@ -0,0 +1,88 @@
+function getComputerChoice() {
+    let compChoiceBasis = Math.random()
+    compChoiceBasis = compChoiceBasis * 100
+    compChoiceBasis = Math.round(compChoiceBasis)
+
+    let x = compChoiceBasis
+    console.log(x)
+
+    if (x <= 33) {
+        x = "Rock"
+    }
+    else if (x <= 66) {
+        x = "Paper"
+    }
+    else {
+        x = "Scissors"
+    }
+
+    return x
+
+}
+
+let computerChoice = getComputerChoice()
+
+console.log(computerChoice)
+
+function getHumanChoice() {
+
+    // Asks the user for their input
+
+    let input = prompt("Choose rock, paper, or scissors.")
+
+    // Transforms the user input into proper case
+
+    let inputStart = input.slice(0, 1)
+    inputStart = inputStart.toUpperCase()
+    let inputEnd = input.slice(1)
+    inputEnd = inputEnd.toLowerCase()
+
+    input = inputStart + inputEnd
+    console.log(input)
+
+    // Checks the input variable to see if it is valid, if not it will return an error.
+
+    if (input == "Rock" || input == "Paper" || input == "Scissors") {
+        return input
+    } else {
+        console.log("Error! Input must be 'Rock', 'Paper', or 'Scissors'")
+    }
+        
+}
+
+let humanChoice = getHumanChoice()
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+
+    let winner = ""
+
+    if (humanChoice == computerChoice) {
+        winner = "Tie"
+    } else if (humanChoice == "Rock") {
+        if (computerChoice == "Scissors"){
+            winner = "Human"
+        } else {
+            winner = "Computer"
+        }
+    } else if (humanChoice == "Scissors") {
+        if (computerChoice == "Paper"){
+            winner = "Human"
+        } else {
+            winner = "Computer"
+        }
+    } else { // humanChoice = Paper
+        if (computerChoice == "Rock"){
+            winner =="Human"
+        } else {
+            winner = "Computer"
+        }
+    }
+
+    return winner
+}
+
+let roundWinner = playRound(humanChoice, computerChoice)
+console.log(roundWinner)
